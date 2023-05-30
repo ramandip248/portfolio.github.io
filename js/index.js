380fc7b66e35937c491ded0fb429fb2a4892db8e
ramandip248 <89860029+ramandip248@users.noreply.github.com>
2023-05-30 19:03:29 +0530

    v2 index.js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,25 @@ var header = document.querySelector('header');
 var menu = document.querySelector('#menu-icon');
 var navbar = document.querySelector('.navbar');
 var darkmode = document.querySelector('#darkmode');
+var hideMenu = document.querySelector('.hideMenu');
 
-
-menu.onclick =()=>{
-    navbar.classList.toggle('active');
+menu.onclick=()=>{
+    if(navbar.classList.contains('active')){
+        navbar.classList.remove('active');
+    }
+    else{
+        navbar.classList.add('active');
+    }
+   
 }
 
+navbar.addEventListener('click', (event) => {
+    if (event.target.tagName.toLowerCase() === 'a' && event.target.classList.contains('hideMenu')) {
+        navbar.classList.remove('active');;
+    }
+    return;
+  });
+  
 darkmode.onclick =()=>{
     if(darkmode.classList.contains('fa-moon')){
         darkmode.classList.replace('fa-moon','fa-sun');
